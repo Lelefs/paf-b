34f76d4c0a0a4fc5075edcc8c6ee9d7f8af04b9c
Leandro Finochio <lefinochio@hotmail.com>
2025-05-07 16:32:56 -0300

    Correção ao salvar data

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,15 @@
 module.exports = async function formatDate(string) {
+  const now = new Date();
   const date = string.split('/');
-  const newDate = new Date(`${date[1]}/${date[0]}/${date[2]}`);
+  const newDate = new Date(
+    date[2],
+    Number(date[1]) - 1,
+    date[0],
+    now.getHours(),
+    now.getMinutes(),
+    now.getSeconds(),
+    now.getMilliseconds(),
+  );
+
   return new Date(newDate);
 };
